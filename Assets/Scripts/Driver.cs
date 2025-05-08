092c0d8b662bfc00c56c530c1a95c141361112db
Gee Chong Shek <geeshek95@outlook.com>
2025-05-08 21:40:08 +0100

    Added framerate independent

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -11,9 +11,11 @@ public class Driver : MonoBehaviour
 
     void Update()
     {
-        // player can press from -1 to +1
-        var steerAmount = Input.GetAxis("Horizontal") * steerSpeed;
-        var moveAmount = Input.GetAxis("Vertical") * moveSpeed;
+        // player can press from -1 (left) to +1 (right)
+        // Positive Z rotation = counter-clockwise (left turn)
+        // Negative Z rotation = clockwise (right turn)
+        var steerAmount = Input.GetAxis("Horizontal") * steerSpeed  * Time.deltaTime;
+        var moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
     }
