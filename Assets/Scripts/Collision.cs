85748dd2be0928dcc34d4f5b7d262bee82b48964
Gee Chong Shek <geeshek95@outlook.com>
2025-05-09 15:07:28 +0100

    Added trigger

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -1,9 +1,15 @@
+using System;
 using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
-  private void OnCollisionEnter2D(Collision2D collision2D)
+  private void OnCollisionEnter2D(Collision2D other)
   {
     Debug.Log("that hurts, drive better");
+  }
+
+  private void OnTriggerEnter2D(Collider2D other)
+  {
+    Debug.Log("power up triggered");
   }
 }
