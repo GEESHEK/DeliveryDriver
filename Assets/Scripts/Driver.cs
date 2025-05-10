fceeb14601b1d4823b1871879d77371c6cb81ec2
Gee Chong Shek <geeshek95@outlook.com>
2025-05-10 13:51:34 +0100

    Added booster and slow speed

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -2,21 +2,34 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] private float steerSpeed = 0.1f;
-    [SerializeField] private float moveSpeed = 0.01f;
+    [SerializeField] private float steerSpeed = 200f;
+    [SerializeField] private float moveSpeed = 20f;
+    [SerializeField] private float slowSpeed = 15f;
+    [SerializeField] private float boostSpeed = 30f;
 
-    void Start()
-    {
-    }
-
-    void Update()
+    private void Update()
     {
         // player can press from -1 (left) to +1 (right)
         // Positive Z rotation = counter-clockwise (left turn)
         // Negative Z rotation = clockwise (right turn)
-        var steerAmount = Input.GetAxis("Horizontal") * steerSpeed  * Time.deltaTime;
+        var steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
         var moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Slow speed");
+        moveSpeed = slowSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Booster")
+        {
+            Debug.Log("Booster activated");
+            moveSpeed = boostSpeed;
+        }
     }
 }
